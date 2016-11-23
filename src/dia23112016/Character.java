8e496b76b40291204578c3acbc18d437dd359c0d
UserPL135Pc13 <UserPL135Pc13@SLFRPL135PC13.ensino.atec.local>
2016-11-23 13:04:38 +0000

    Atualizacao 1 - dia 23/11/2016

---
+++ src/dia23112016/Character.java
@@ -0,0 +1,37 @@
+package dia23112016;
+
+public class Character {
+
+	int ataque, defesa, saude;
+	
+	public int getAtaque() {
+		return ataque;
+	}
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+	public int getDefesa() {
+		return defesa;
+	}
+
+	public void setDefesa(int defesa) {
+		this.defesa = defesa;
+	}
+
+	public int getSaude() {
+		return saude;
+	}
+
+	public void setSaude(int saude) {
+		this.saude = saude;
+	}
+
+	public Character() {
+		// TODO Auto-generated constructor stub
+
+	}
+	
+
+}
---
+++ src/dia23112016/Wololo.java
@@ -0,0 +1,19 @@
+package dia23112016;
+
+public class Wololo extends Character{
+	
+	int convertionRate;
+	
+	public int getConvertionRate() {
+		return convertionRate;
+	}
+
+	public void setConvertionRate(int convertionRate) {
+		this.convertionRate = convertionRate;
+	}
+
+	public Wololo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
